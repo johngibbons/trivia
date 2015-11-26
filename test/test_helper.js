c0de8637372b717b7670354b069226a3f4a370cd
John Gibbons <johngibbons10@gmail.com>
2015-11-25 16:06:25 -0800

    set up tests to work with jsdom

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,4 +1,17 @@
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
+import jsdom from 'jsdom';
+
+const doc = jsdom.dom('<?doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
 
 chai.use(chaiImmutable);
